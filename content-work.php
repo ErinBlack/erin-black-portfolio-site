3baf4aea996d0fa100b96f644117dcfd6eb5f58f
Erin Black <erinblackdesign@gmail.com>
2017-08-15 20:45:48 -0500

    got limited info to display on the main work page

--- content-work.php
+++ content-work.php
@@ -12,30 +12,7 @@
 	<div class="title-image">
 			<img src="<?php the_field('image'); ?>" alt="Example image of <?php the_title(); ?>">
 	</div>
-	<div class="text-wrapper">
-		<div class="entry-content">
-			<p><?php the_field('description'); ?></p>
-		</div><!-- .entry-content -->
-		<div class="container">
-			<div class="two-grid">
-				<a href="<?php the_field('github_link'); ?>">
-					<img class="two-grid-img"
-					src="http://localhost:8888/my-theme/wp-content/uploads/2017/08/github.png"
-					alt="view code on github">
-				</a>
-				<a href="<?php the_field('github_link'); ?>"><h3>Github of This Project</h3></a>
-			</div>
-			<div class="two-grid">
-				<a href="<?php the_field('hosted'); ?>">
-					<img class="two-grid-img"
-					src="http://localhost:8888/my-theme/wp-content/uploads/2017/08/computer-icon.png"
-					alt="view code on github">
-				</a>
-				<a href="<?php the_field('hosted'); ?>"><h3>See Application</h3></a>
-			</div>
-		</div>
-
-	</div>
+	
 
 
 </article><!-- #post -->
--- single-work.php
+++ single-work.php
@@ -2,13 +2,41 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<h1 class="entry-title">
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</h1>
+	<div class="title-image">
+			<img src="<?php the_field('image'); ?>" alt="Example image of <?php the_title(); ?>">
+	</div>
+	<div class="text-wrapper">
+		<div class="entry-content">
+			<p><?php the_field('description'); ?></p>
+		</div><!-- .entry-content -->
+		<div class="container">
+			<div class="two-grid">
+				<a href="<?php the_field('github_link'); ?>">
+					<img class="two-grid-img"
+					src="http://localhost:8888/my-theme/wp-content/uploads/2017/08/github.png"
+					alt="view code on github">
+				</a>
+				<a href="<?php the_field('github_link'); ?>"><h3>Github of This Project</h3></a>
+			</div>
+			<div class="two-grid">
+				<a href="<?php the_field('hosted'); ?>">
+					<img class="two-grid-img"
+					src="http://localhost:8888/my-theme/wp-content/uploads/2017/08/computer-icon.png"
+					alt="view code on github">
+				</a>
+				<a href="<?php the_field('hosted'); ?>"><h3>See Application</h3></a>
+			</div>
+		</div>
 
-			<?php get_template_part( 'content', 'work' ); ?>
+	</div>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+
+</article><!-- #post -->
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>

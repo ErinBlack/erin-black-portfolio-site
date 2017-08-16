73daecc486902c1b1b96ef23e9cee5b9f702c14e
Erin Black <erinblackdesign@gmail.com>
2017-08-16 10:53:54 -0500

    got screenshots displayed and styled

--- single-work.php
+++ single-work.php
@@ -13,6 +13,17 @@ get_header(); ?>
 		<div class="entry-content">
 			<p><?php the_field('description'); ?></p>
 		</div><!-- .entry-content -->
+		<div class="container screenshotContainer">
+			<div class="three-grid">
+				<img class="three-grid-img" src="<?php the_field('screenshot_1') ?>" alt="">
+			</div>
+			<div class="three-grid">
+				<img class="three-grid-img" src="<?php the_field('screenshot_2') ?>" alt="">
+			</div>
+			<div class="three-grid">
+				<img  class="three-grid-img" src="<?php the_field('screenshot_3') ?>" alt="">
+			</div>
+		</div>
 		<div class="container">
 			<div class="two-grid">
 				<a href="<?php the_field('github_link'); ?>">
